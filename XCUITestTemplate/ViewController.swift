e740acf1070c26f8a4526b1003a74fef2c974277
Santhosh <santhosh_nampally@epam.com>
2019-05-24 11:51:49 +0530

    inital commit with basic Xcode project

---
+++ XCUITestTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCUITestTemplate
+//
+//  Created by Santhosh Nampally on 24/05/19.
+//  Copyright © 2019 Santhosh Nampally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
